3b2ffc4a22b74dc94ccc8d429d5d9f10cc4dd57f
darshan <darshan.patel.tops@gmail.com>
2023-05-31 16:02:28 +0530

    First Commit

--- PHP/03Casesensitive.php
+++ PHP/03Casesensitive.php
@@ -5,7 +5,7 @@
 $Name = "darshan"; //variables are case sensitive
 $NAME = "Mohammad"; //variables are case sensitive
 $name = 415 + 5; //variables are case sensitive
-$salary = 1000000000;
+$salary = 1000000000.50;
 
 // echo "hello this is $Name and he is $name"; // Double  quotation will allow variable
 // echo "<br>";
@@ -19,7 +19,7 @@ ECHO "$name";  //inbuilt functions are not case sensitive
 echo "<br>";
 eCHO "$NAME";  //inbuilt functions are not case sensitive
 echo "<br>";
-EcHo "$Name";  //inbuilt functions are not case sensitive
+EcHo "$Name <br> $salary";  //inbuilt functions are not case sensitive
 
 
 ?>
