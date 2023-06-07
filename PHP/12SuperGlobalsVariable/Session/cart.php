b1cc17a6f05ada6520006c879b47f30b38a9cec7
darshan <darshan.patel.tops@gmail.com>
2023-06-07 15:52:08 +0530

    First Commit

--- PHP/12SuperGlobalsVariable/Session/cart.php
+++ PHP/12SuperGlobalsVariable/Session/cart.php
@@ -5,4 +5,20 @@ require_once('header.php');
 
 ?>
 
+<?php
+
+if(isset($_SESSION['cartdata']))
+{
+    foreach($_SESSION['cartdata'] as $data)
+    {
+       ?>
+        <tr>
+            <td><?php echo $data['price']?></td>
+        </tr>       
+       <?php
+    }
+}
+
+?>
+
 <h1>Cart Page</h1>
--- PHP/12SuperGlobalsVariable/Session/product.php
+++ PHP/12SuperGlobalsVariable/Session/product.php
@@ -2,7 +2,11 @@
 // session_start();
 require_once('header.php');
 echo"<pre>";
-print_r($_SESSION);
+if(isset($_REQUEST['add']))
+{
+    print_r($_REQUEST);
+}
+// print_r($_SESSION);
 echo"</pre>";
 if(isset($_SESSION["products"]))
 {
@@ -11,13 +15,21 @@ if(isset($_SESSION["products"]))
 
     foreach($products as $product)
     {
-        echo '<div class="product">';
+    ?>
+        <form action="" method="post">
+
+            <?php
+        echo '<div  class="mx-auto" style="width: 200px;" mt-5>';
         echo '<img src="'.$product["image"].'"alt="product image" height="100px" width="100px">';
         echo '<p>product price:'. $product["price"].'</p>';
+        echo "<input name='price' type=hidden value= ".$product['price'] .">";
+        echo '<button type="submit" class="btn btn-sm  btn-primary" name="add"> Add To Cart <button>';
         // echo"";
         echo '</div>';
-    }
+    }?>
 
+    </form>
+<?php 
 
 }
 else
