1a25ba17fdba6d88d29e6e92dfdafb641be3ff9a
darshan <darshan.patel.tops@gmail.com>
2023-06-08 14:56:08 +0530

    First Commit

--- PHP/12SuperGlobalsVariable/Session/product.php
+++ PHP/12SuperGlobalsVariable/Session/product.php
@@ -1,42 +1,45 @@
 <?php
-// session_start();
 require_once('header.php');
-echo"<pre>";
-if(isset($_REQUEST['add']))
+echo "<pre>";
+if (isset($_REQUEST['add'])) 
 {
     print_r($_REQUEST);
+
+    // if (!isset($_SESSION['cartdata'])) {
+    //     $_SESSION['cartdata'][] = array();
+    // }
+
+    $productData = array("product" => array("price" => $_REQUEST['price']));
+
+    $_SESSION['cartdata'][] = $productData;
+
+    print_r($_SESSION['cartdata']);
 }
-// print_r($_SESSION);
-echo"</pre>";
-if(isset($_SESSION["products"]))
+echo "</pre>";
+if (isset($_SESSION["products"])) 
 {
     $products = $_SESSION["products"];
 
-
-    foreach($products as $product)
+    foreach ($products as $product) 
     {
-    ?>
+        ?>
         <form action="" method="post">
 
             <?php
-        echo '<div  class="mx-auto" style="width: 200px;" mt-5>';
-        echo '<img src="'.$product["image"].'"alt="product image" height="100px" width="100px">';
-        echo '<p>product price:'. $product["price"].'</p>';
-        echo "<input name='price' type=hidden value= ".$product['price'] .">";
-        echo '<button type="submit" class="btn btn-sm  btn-primary" name="add"> Add To Cart <button>';
-        // echo"";
-        echo '</div>';
-    }?>
+            echo '<div class="mx-auto" style="width: 200px;" mt-5>';
+            echo '<img src="' . $product["image"] . '" alt="product image" height="100px" width="100px">';
+            echo '<p>product price:' . $product["price"] . '</p>';
+            echo "<input name='price' type='hidden' value='" . $product['price'] . "'>";
+            echo '<button type="submit" class="btn btn-sm btn-primary" name="add">Add To Cart</button>';
+            echo '</div>';
+            ?>
 
-    </form>
-<?php 
-
-}
-else
+        </form>
+        <?php
+    }
+} 
+else 
 {
     echo "no product available";
 }
 ?>
-
-<!-- <h1>Product Page</h1> -->
-
