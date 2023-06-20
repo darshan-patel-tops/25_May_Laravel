334fc5ace9a49d07d6822e5b474f9369c07295c3
darshan <darshan.patel.tops@gmail.com>
2023-06-20 16:30:44 +0530

    First Commit

--- PHP/13Array/07Combine_Merge.php
+++ PHP/13Array/07Combine_Merge.php
@@ -10,4 +10,23 @@ print_r($newarray);
 echo "<br>";
 print_r(array_merge($name,$age));
 //It will overwrite the last value
+
+
+
+echo "<br>";
+$a = array("test"=>10,20,30,40);
+$b = array("test"=>'a','b','c','d');
+
+echo "<br>";
+
+echo "array merge example";
+echo "<br>";
+echo "<br>";
+
+echo "<pre>";
+print_r(array_merge($a,$b));
+echo "array merge recursive";
+echo "<br>";
+print_r(array_merge_recursive($a,$b));
+echo "</pre>";
 ?>
