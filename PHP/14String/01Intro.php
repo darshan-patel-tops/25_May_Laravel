6a21edfa677719bf4d9ff90d3b9ac2e7ba049c1e
darshan <darshan.patel.tops@gmail.com>
2023-06-21 16:30:40 +0530

    first

---
+++ PHP/14String/01Intro.php
@@ -0,0 +1,17 @@
+<?php 
+
+    echo "hello this is the practical of string <br>";
+    
+    $name = "Name is mohammed";
+    $string = "this is a string";
+
+    echo "<br>";
+    echo $name;
+    echo "<br>";
+    echo $name[1];
+
+
+
+
+
+?>
