fb23bdfc42aed6e522dd0c368aa1fedbc797e634
darshan <darshan.patel.tops@gmail.com>
2023-06-26 16:13:37 +0530

    first

--- PHP/16OOPS/01Function.php
+++ PHP/16OOPS/01Function.php
@@ -1,9 +1,14 @@
 <?php
 
+//User Defined Function
+//System Defined Function or Pre-Defined Function
+//int main() is a udf
+function call()
+{
+    echo "called";
+}
 
-
-
-
+call();
 
 
 
---
+++ PHP/16OOPS/02AccessModifier.php
@@ -0,0 +1,11 @@
+<?php
+
+
+// public           public is like water
+// private          private is like juice
+// protected        protected is like expensive black water
+
+
+
+
+?>
