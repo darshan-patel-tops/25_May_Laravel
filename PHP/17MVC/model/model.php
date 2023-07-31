f023a0ae22a5b04f8ea2916a9e3f4e6078625c06
darshan <darshan.patel.tops@gmail.com>
2023-07-31 15:50:55 +0530

    first

---
+++ PHP/17MVC/model/model.php
@@ -0,0 +1,52 @@
+<?php
+
+
+
+
+class model
+{
+
+    public $connection;
+    public function __construct()
+    {
+       
+        try {
+            $this->connection = new mysqli("localhost","root","","25_may_laravel");
+            echo "success";
+        } catch (\Throwable $th) {
+            echo "Something went wrong";
+        }
+        
+    }
+
+    public function select()
+    {
+        echo "<br>";
+        $sql = "select * from users";
+        echo $sql;
+        
+        echo "<br>";
+        $sqlex = $this->connection->query($sql);
+        print_r($sqlex);
+
+        if($sqlex->num_rows>0)
+        {   
+            echo "<pre>";
+            // print_r($sqlex->fetch_object());
+            print_r($sqlex->fetch_all());
+            echo "</pre>";
+        }
+        else
+        {
+            echo "no data found";
+        }
+    }
+
+
+}
+
+
+$object = new model;
+$object->select();
+
+?>
