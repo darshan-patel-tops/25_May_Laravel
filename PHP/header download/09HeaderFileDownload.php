6f62842af58a53a3cc2f11aba7e0f1d61a2a8742
darshan <darshan.patel.tops@gmail.com>
2023-08-14 15:53:24 +0530

    Update

--- PHP/header download/09HeaderFileDownload.php
+++ PHP/header download/09HeaderFileDownload.php
@@ -13,7 +13,31 @@ if (isset($_REQUEST['Download'])) {
     exit();
 }
 ?>
+<?php
+$file_path = 'movie.mp4';
 
+if (file_exists($file_path)) {
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="' . basename($file_path) . '"');
+    header('Content-Length: ' . filesize($file_path));
+    
+    // Open the file for reading
+    $file = fopen($file_path, 'rb');
+    
+    // Output the file in chunks
+    while (!feof($file)) {
+        echo fread($file, 1024 * 1024); // Output 1MB chunks (adjust as needed)
+        ob_flush();
+        flush();
+    }
+    
+    fclose($file);
+    exit;
+} else {
+    echo "File not found.";
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
